6bbd9c1190e21e665a6322bc8b5009b9b306927f
Aniket1398 <aniket13kumar@gmail.com>
2022-08-04 23:32:46 +0530

    Time: 13 ms (60.42%), Space: 7.3 MB (77.60%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int sum=0,num=0;
+        vector<int>m1(256,0),m2(256,0);
+        for(int i=0;i<s.size();++i)
+        {
+            m1[s[i]]++;
+        }
+        for(int i=0;i<t.size();++i)
+        {
+           m2[t[i]]++;
+        }
+       return m1==m2;
+    }
+};
